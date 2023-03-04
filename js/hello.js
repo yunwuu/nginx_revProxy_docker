9ef895c9cd326c159b2f3a9586c1f3d8d488118f
云雾 <123147018+yunwuu@users.noreply.github.com>
2023-03-04 21:01:23 +0800

    Update JS

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,13 @@
-console.log('hello');
+console.log('Google Mirror JS');
+console.log('Powered by Yunwu');
+
+
+const key = "GF3DA";
+
+
+function verify() {
+    if(!document.cookie || !document.cookie.key || document.cookie.key != key) {
+        document.write("403Forbidden");
+        return;
+    }
+}
